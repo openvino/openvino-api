5ee2cee0a6b8d12e41170da5133a753af2f5ba15
jestape <jestape@protonmail.com>
2020-04-16 13:44:15 +0200

    add grouping data

--- src/app/handler/sensordata.go
+++ src/app/handler/sensordata.go
@@ -32,7 +32,14 @@ func GetSensorDataMonth(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
 
 	sensordata := []model.SensorData{}
 
-	db.Where("MONTH(timestamp) = ? AND YEAR(timestamp) = ?", month, year).Find(&sensordata)
+	db.Select("min(timestamp) as timestamp, sensor_id," +
+				"avg(humidity2) as humidity2, avg(humidity1) as humidity1," +
+				"avg(humidity05) as humidity05, avg(humidity005) as humidity005," +
+				"max(wind_velocity) as wind_velocity, max(wind_gust) as wind_gust," +
+				"avg(wind_direction) as wind_direction, avg(pressure) as pressure," +
+				"max(rain) as rain, avg(temperature) as temperature," +
+				"avg(humidity) as humidity").Group("day(timestamp), sensor_id").Having("year(timestamp) = ? AND month(timestamp) = ?", year, month).Find(&sensordata)
+
 	respondJSON(w, http.StatusOK, sensordata)
 
 }
@@ -43,7 +50,14 @@ func GetSensorDataYear(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
 
 	sensordata := []model.SensorData{}
 
-	db.Where("YEAR(timestamp) = ?", year).Find(&sensordata)
+	db.Select("min(timestamp) as timestamp, sensor_id," +
+				"avg(humidity2) as humidity2, avg(humidity1) as humidity1," +
+				"avg(humidity05) as humidity05, avg(humidity005) as humidity005," +
+				"max(wind_velocity) as wind_velocity, max(wind_gust) as wind_gust," +
+				"avg(wind_direction) as wind_direction, avg(pressure) as pressure," +
+				"max(rain) as rain, avg(temperature) as temperature," +
+				"avg(humidity) as humidity").Group("month(timestamp), sensor_id").Having("year(timestamp) = ?", year).Find(&sensordata)
+
 	respondJSON(w, http.StatusOK, sensordata)
 
 }
