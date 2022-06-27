4b2bd7cbfa9557f9f4f06d0cb9d99e2b2389ba97
Marc Baqué <marc@bloock.com>
2022-06-27 18:29:17 +0200

    fix language path

--- src/controller/language/handler.go
+++ src/controller/language/handler.go
@@ -2,16 +2,17 @@ package language
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
-	customHTTP "github.com/openvino/openvino-api/src/http"
 	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
+	customHTTP "github.com/openvino/openvino-api/src/http"
 )
 
 func GetLanguage(w http.ResponseWriter, r *http.Request) {
 	lang := mux.Vars(r)["lang"]
-	jsonFile, err := os.Open("/languages/" + lang + ".json")
+	jsonFile, err := os.Open("./languages/" + lang + ".json")
 	if err != nil {
 		customHTTP.NewErrorResponse(w, http.StatusBadRequest, err.Error())
 		return
